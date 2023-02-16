e9e421762f510290ed715877bfd5ad85ec9474c4
r4vsdev <r4vsdev@gmail.com>
2023-02-16 19:05:12 -0300

    Separate the method shift_letter into its lowercase and uppercase versions to improve readability

--- caesar.rb
+++ caesar.rb
@@ -1,17 +1,29 @@
+# frozen_string_literal: true
+
 def shift_letter(letter, shift)
-  unless letter.ord.between?(65, 90) or letter.ord.between?(97, 122)
-    return letter
-  end
-  
+  return letter unless letter.ord.between?(65, 90) || letter.ord.between?(97, 122)
+
   if letter == letter.upcase
-    (letter.ord + shift % 26) > 90 ? (letter.ord + shift % 26 - 26).chr : (letter.ord + shift % 26).chr
+    shift_uppercase(letter, shift)
   else
-    (letter.ord + shift % 26) > 122 ? (letter.ord + shift % 26 - 26).chr : (letter.ord + shift % 26).chr
+    shift_lowercase(letter, shift)
   end
+end
+
+def shift_lowercase(letter, shift)
+  return letter unless letter.ord.between?(65, 90) || letter.ord.between?(97, 122)
+
+  (letter.ord + shift % 26) > 122 ? (letter.ord + shift % 26 - 26).chr : (letter.ord + shift % 26).chr
+end
+
+def shift_uppercase(letter, shift)
+  return letter unless letter.ord.between?(65, 90) || letter.ord.between?(97, 122)
+
+  (letter.ord + shift % 26) > 90 ? (letter.ord + shift % 26 - 26).chr : (letter.ord + shift % 26).chr
 end
 
 def caesar(word, shift)
   array = word.split('')
-  result = array.map {|letter| shift_letter(letter, shift)}
+  result = array.map { |letter| shift_letter(letter, shift) }
   result.join('')
 end
